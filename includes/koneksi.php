cc62342fb81e3e47047f4187f82a2f0c158f0211
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2026-09-20 23:58:40 +0700

    tes

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $port = "5432";
 $db   = "simpus_mini";
 $user = "postgres";
-$pass = "postgres";
+$pass = "180906";
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
